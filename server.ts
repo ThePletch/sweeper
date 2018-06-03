8936e3f1a8a92df060c39f22976b58d30eeb0b6c
Glitch (hello-typescript) <none>
2018-06-03 02:53:21 +0000

    📖👩‍❤️‍💋‍👩 Checkpoint ./server.ts:334152/958 ./package.json:334152/1415

---
+++ server.ts
@@ -0,0 +1,25 @@
+// server ts (compiles to server.js)
+// where your node app starts
+
+// init project
+import * as express from 'express';
+let app = express();
+
+declare let __dirname: string;
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get('/', function (request: express.Request, response: express.Response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+// listen for requests :)
+let listener = app.listen(process.env.PORT, function() {
+  // console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + 
+});

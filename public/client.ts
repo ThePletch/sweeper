58ff4462df1e991e15bc0b7e809150238408740e
Glitch (hello-typescript) <none>
2023-05-08 21:48:32 +0000

    🏔🏖 Checkpoint ./package.json:334152/4356 ./public/client.ts:334152/2566 ./server.ts:334152/1455 ./views/index.html:334152/3703

--- public/client.ts
+++ public/client.ts
@@ -1,44 +1,43 @@
 // client-side ts (compiles to client.js)
 // run by the browser each time your view template is loaded
 (() => {
-  console.log('hello world :o');
-  
+  console.log("hello world :o");
+
   // our default array of dreams
   const dreams: string[] = [
-    'Find and count some sheep',
-    'Climb a really tall mountain',
-    'Wash the dishes'
+    "Find and count some sheep",
+    "Climb a really tall mountain",
+    "Wash the dishes",
   ];
-  
+
   // define variables that reference elements on our page
-  const dreamsList = document.getElementById('dreams');
+  const dreamsList = document.getElementById("dreams");
   const dreamsForm = document.forms[0];
-  const dreamInput = dreamsForm.elements['dream'];
-  
+  const dreamInput = dreamsForm.elements["dream"];
+
   // a helper function that creates a list item for a given dream
   const appendNewDream = (dream: string) => {
-    const newListItem = document.createElement('li');
+    const newListItem = document.createElement("li");
     newListItem.innerHTML = dream;
     dreamsList.appendChild(newListItem);
-  }
-  
+  };
+
   // iterate through every dream and add it to our page
-  dreams.forEach( (dream: string) => {
+  dreams.forEach((dream: string) => {
     appendNewDream(dream);
   });
-  
+
   // listen for the form to be submitted and add a new dream when it is
-  dreamsForm.onsubmit = e => {
+  dreamsForm.onsubmit = (e) => {
     // stop our form submission from refreshing the page
     e.preventDefault();
-    
+
     // get dream value and add it to the list
     dreams.push(dreamInput.value);
     appendNewDream(dreamInput.value);
-  
-    // reset form 
-    dreamInput.value = '';
+
+    // reset form
+    dreamInput.value = "";
     dreamInput.focus();
   };
-  
 })();
--- server.ts
+++ server.ts
@@ -2,22 +2,22 @@
 // where your node app starts
 
 // init project
-import * as express from 'express';
+import * as express from "express";
 const app: express.Application = express();
 
-// we've started you off with Express, 
+// we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request: express.Request, response: express.Response) => {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/", (request: express.Request, response: express.Response) => {
+  response.sendFile(__dirname + "/views/index.html");
 });
 
 // listen for requests :)
-const port: string = process.env.PORT || '3000';
+const port: string = process.env.PORT || "3000";
 app.listen(port, () => {
   console.log(`Your app is listening on port ${port}`);
 });
